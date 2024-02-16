67c0d70d076f48875bccea08970d060a0d1f8646
Soham Ovhal <ovhalsoham@gmail.com>
2024-02-17 02:20:06 +0530

    BallonFloat

--- Assets/Script/BallonSpawner.cs
+++ Assets/Script/BallonSpawner.cs
@@ -6,22 +6,28 @@ public class BallonSpawner : MonoBehaviour
 {
     public GameObject[] ballons;
     public Transform spawner;
+    [SerializeField]private float  spawnInterval;
+    [SerializeField]private float ballonForce;
     // Start is called before the first frame update
     void Start()
     {
         spawner = GetComponent<Transform>();
+        ballonForce = 25f;
+        spawnInterval = 6f;
+        InvokeRepeating("balloonSpawner", 0f, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        balloonSpawner();
+        
     }
 
-    IEnumerator balloonSpawner()
+    void balloonSpawner()
     {
         GameObject randomBalloon = ballons[Random.Range(0, ballons.Length)];
-        Instantiate(randomBalloon, spawner.position, Quaternion.identity);
-        yield return new WaitForSeconds(2f);
+        GameObject newBallon = Instantiate(randomBalloon, spawner.position, randomBalloon.transform.rotation);
+        Rigidbody ballonRigidBody = newBallon.GetComponent<Rigidbody>();
+        ballonRigidBody.AddForce(Vector3.up * ballonForce, ForceMode.Acceleration);
     }
 }
